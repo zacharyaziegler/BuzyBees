93c5865d3763f0aa76ea1737b04a03ce9463beaf
Zachary Ziegler <ziegza@farmingdale.edu>
2023-11-01 18:04:59 -0400

    Added dependencies for CalendarFX, created Schedule view and controller

---
+++ src/main/java/csc/buzybees/ScheduleController.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package csc.buzybees;
+
+import javafx.scene.layout.AnchorPane;
+
+/**
+ *
+ * @author zman1
+ */
+public class ScheduleController extends AnchorPane {
+    
+    
+    
+}
