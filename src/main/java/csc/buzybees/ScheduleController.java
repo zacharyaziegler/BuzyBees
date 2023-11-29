58a49d3da4c7633eb5bbff51bb815db01b8fc8a7
Zachary Ziegler <ziegza@farmingdale.edu>
2023-11-28 19:30:11 -0500

    Final Schedule skeleton

--- src/main/java/csc/buzybees/ScheduleController.java
+++ src/main/java/csc/buzybees/ScheduleController.java
@@ -6,12 +6,15 @@ package csc.buzybees;
 
 import java.io.IOException;
 import java.time.LocalDate;
+import java.time.Month;
 import java.time.YearMonth;
 import java.time.format.DateTimeFormatter;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -59,36 +62,56 @@ public class ScheduleController extends AnchorPane {
     private Button leaveButton;
 
     private YearMonth currentYearMonth = YearMonth.now();
+//    private YearMonth currentYearMonth = YearMonth.of(2023, Month.DECEMBER); // debugging purposes
     private GridPane scheduleGrid;
     private Label monthLabel = new Label();
 
     @FXML
     public void initialize() {
+        String[] dayNames = {"Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun"}; // day labels on top of schedule grid
+        GridPane dayLabels = new GridPane();
+        for (int i = 0; i < 7; i++) {
+            ColumnConstraints column = new ColumnConstraints(150); // The width should match the scheduleGrid's column width
+            dayLabels.getColumnConstraints().add(column);
+        }
+        for (int i = 0; i < dayNames.length; i++) {
+            Label lblDay = new Label(dayNames[i]);
+            lblDay.setAlignment(Pos.BOTTOM_LEFT);
+            lblDay.setMaxWidth(Double.MAX_VALUE); // Allows the label to grow to fill the cell
+            GridPane.setHalignment(lblDay, HPos.CENTER); // Center align the day labels horizontally
+            GridPane.setMargin(lblDay, new Insets(0, 0, 0, 0));
+            // Set the label to fill the entire cell for consistent alignment
+            lblDay.setStyle("-fx-font-weight: bold; -fx-text-alignment: center;");
+            dayLabels.add(lblDay, i, 0); // Add the label to the corresponding column
+        }
+        dayLabels.setGridLinesVisible(true);
+        dayLabels.setVgap(0);
+        dayLabels.setHgap(0);
 
-        VBox scheduleLayout = new VBox(); // 10 is the spacing between elements
+        VBox scheduleLayout = new VBox(); // no spacing between elements in order to create grid lines
         updateMonthLabel(currentYearMonth);
+
         // Initialize the calendar grid and other components
         scheduleGrid = new GridPane();
-        scheduleGrid.setMinSize(1100, 375);
-        scheduleGrid.setPrefSize(1100, 375); // Set preferred size as needed
-        scheduleGrid.setMaxSize(1100, 375);
 
         for (int i = 0; i < 7; i++) { // set contraints on rows and columns of GridPane
-            ColumnConstraints column = new ColumnConstraints(150); // Set the preferred width of columns
-            RowConstraints row = new RowConstraints(50); // Set the preferred height of rows
+            ColumnConstraints column = new ColumnConstraints(150); // Set the width of columns
+            RowConstraints row = new RowConstraints(75); // Set the height of rows
             scheduleGrid.getColumnConstraints().add(column);
             scheduleGrid.getRowConstraints().add(row);
         }
 
-        scheduleGrid.setGridLinesVisible(true);
-        scheduleGrid.setVgap(10);
-        scheduleGrid.setHgap(10);
-        scheduleGrid.setStyle("-fx-border-color: red;");
+        scheduleGrid.setVgap(0);
+        scheduleGrid.setHgap(0);
+        dayLabels.setPadding(new Insets(0, 0, 0, 0));
+        scheduleGrid.setPadding(new Insets(0, 0, 0, 0));
+//        scheduleGrid.setStyle("-fx-border-color: red;"); // debugging purposes
         updateSchedule();
-        scheduleLayout.getChildren().addAll(monthLabel, scheduleGrid);
-        AnchorPane.setTopAnchor(scheduleLayout, 100.0); // Adjust this value as needed
+        scheduleLayout.getChildren().addAll(monthLabel, dayLabels, scheduleGrid);
+        AnchorPane.setTopAnchor(scheduleLayout, 100.0);
         AnchorPane.setLeftAnchor(scheduleLayout, 10.0);
         AnchorPane.setRightAnchor(scheduleLayout, 10.0);
+        AnchorPane.setBottomAnchor(scheduleLayout, 0.0);
         scheduleContainer.getChildren().add(scheduleLayout); // Add the calendar to the container
     }
 
@@ -96,25 +119,82 @@ public class ScheduleController extends AnchorPane {
         scheduleGrid.getChildren().clear(); // Clear the existing calendar cells
 
         LocalDate calendarDate = LocalDate.of(currentYearMonth.getYear(), currentYearMonth.getMonthValue(), 1);
-        // Fill in the calendar grid with day numbers
         int dayOfWeekOfFirst = calendarDate.getDayOfWeek().getValue();
         int daysInMonth = currentYearMonth.lengthOfMonth();
 
-        int column = 0;
-        int row = 1;
+        // Subtract 1 from dayOfWeekOfFirst so that Monday (1) becomes 0, Tuesday (2) becomes 1, ...
+        int column = dayOfWeekOfFirst - 1;
 
-        // Fill grid with days
-        for (int i = 1; i <= daysInMonth; i++) {
-            if (i == 1) {
-                column = dayOfWeekOfFirst - 1; // Adjust for zero based index
-            } else if (column == 6) {
-                column = 0;
-                row++;
-            } else {
-                column++;
-            }
-            scheduleGrid.add(new Label(String.valueOf(i)), column, row);
+        // Add empty labels for days before the first day of the month 
+        for (int i = 0; i < column; i++) {
+            Label emptyLabel = new Label("");
+            emptyLabel.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+            emptyLabel.setStyle("-fx-border-width: 0 0 1 1; -fx-border-color: black;");
+            scheduleGrid.add(emptyLabel, i, 0); // Add empty labels to the first row
         }
+
+        // Add labels for the days of the month starting from the correct column
+        for (int day = 1; day <= daysInMonth; day++) {
+            Label dayOfMonth = new Label(String.valueOf(day));
+            dayOfMonth.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+            dayOfMonth.setAlignment(Pos.TOP_LEFT);
+            dayOfMonth.setStyle("-fx-border-width: 0 0 1 1; -fx-border-color: black; -fx-font-weight: bold;");
+
+            // Add day label to the correct cell, wrapping to next line if end of the week is reached
+            scheduleGrid.add(dayOfMonth, column % 7, column / 7);
+            column++; // Move to the next cell
+        }
+
+        // Adds empty nodes after days are completed to simulate full calendar
+        while (column % 7 != 0) {
+            Label emptyLabel = new Label("");
+            emptyLabel.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+
+            emptyLabel.setStyle("-fx-border-width: 0 0 1 1; -fx-border-color: black;");
+            scheduleGrid.add(emptyLabel, column % 7, column / 7);
+            column++;
+        }
+        // cleans up unclosed nodes
+        int lastDateRowIndex = getLastDateRowIndex(scheduleGrid); // prevents null pointer exception
+        
+        if (lastDateRowIndex == 4) {
+            getNodeFromGridPane(scheduleGrid, 6, 4).setStyle("-fx-font-weight: bold; -fx-border-width: 0 1 1 1; -fx-border-color: black;");
+            getNodeFromGridPane(scheduleGrid, 6, 3).setStyle("-fx-font-weight: bold; -fx-border-width: 0 1 1 1; -fx-border-color: black;");
+            getNodeFromGridPane(scheduleGrid, 6, 2).setStyle("-fx-font-weight: bold; -fx-border-width: 0 1 1 1; -fx-border-color: black;");
+            getNodeFromGridPane(scheduleGrid, 6, 1).setStyle("-fx-font-weight: bold; -fx-border-width: 0 1 1 1; -fx-border-color: black;");
+            getNodeFromGridPane(scheduleGrid, 6, 0).setStyle("-fx-font-weight: bold; -fx-border-width: 0 1 1 1; -fx-border-color: black;");
+        } else {
+            getNodeFromGridPane(scheduleGrid, 6, 5).setStyle("-fx-font-weight: bold; -fx-border-width: 0 1 1 1; -fx-border-color: black;");
+            getNodeFromGridPane(scheduleGrid, 6, 4).setStyle("-fx-font-weight: bold; -fx-border-width: 0 1 1 1; -fx-border-color: black;");
+            getNodeFromGridPane(scheduleGrid, 6, 3).setStyle("-fx-font-weight: bold; -fx-border-width: 0 1 1 1; -fx-border-color: black;");
+            getNodeFromGridPane(scheduleGrid, 6, 2).setStyle("-fx-font-weight: bold; -fx-border-width: 0 1 1 1; -fx-border-color: black;");
+            getNodeFromGridPane(scheduleGrid, 6, 1).setStyle("-fx-font-weight: bold; -fx-border-width: 0 1 1 1; -fx-border-color: black;");
+            getNodeFromGridPane(scheduleGrid, 6, 0).setStyle("-fx-font-weight: bold; -fx-border-width: 0 1 1 1; -fx-border-color: black;");
+        }
+    }
+
+    private int getLastDateRowIndex(GridPane gridPane) {
+    // Assuming that the dates are Label nodes, we traverse the list in reverse to find the last Label added
+    for (int i = gridPane.getChildren().size() - 1; i >= 0; i--) {
+        Node node = gridPane.getChildren().get(i);
+        // Check if the node is a Label and if it contains a numeric value which represents a date
+        if (node instanceof Label && ((Label) node).getText().matches("\\d+")) {
+            // If it is a date, return the row index
+            Integer rowIndex = GridPane.getRowIndex(node);
+            return rowIndex != null ? rowIndex : 0; // Return 0 if the row index is null
+        }
+    }
+    // Return -1 if no date node is found
+    return -1;
+}
+
+    private Node getNodeFromGridPane(GridPane gridPane, int col, int row) {
+        for (Node node : gridPane.getChildren()) {
+            if (GridPane.getColumnIndex(node) == col && GridPane.getRowIndex(node) == row) {
+                return node;
+            }
+        }
+        return null;
     }
 
     private void updateMonthLabel(YearMonth yearMonth) {
@@ -137,6 +217,11 @@ public class ScheduleController extends AnchorPane {
     @FXML
     private void switchToSchedule(ActionEvent event) throws IOException {
         App.setRoot("ScheduleView");
+    }
+
+    @FXML
+    private void switchToCreateSchedule(ActionEvent event) throws IOException {
+        App.setRoot("create_schedule");
     }
 
 }
