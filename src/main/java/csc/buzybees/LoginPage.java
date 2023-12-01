31a4ac6b1e1ac85116570fd7cb5ed8ed6ad01842
Zachary Ziegler <ziegza@farmingdale.edu>
2023-12-01 15:12:54 -0500

    Updated Login and Register from firestore to Firebase Authentication

--- src/main/java/csc/buzybees/LoginPage.java
+++ src/main/java/csc/buzybees/LoginPage.java
@@ -3,7 +3,14 @@ package csc.buzybees;
 import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.QuerySnapshot;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.concurrent.ExecutionException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -11,6 +18,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import org.json.JSONObject;
 
 public class LoginPage {
 
@@ -24,11 +32,12 @@ public class LoginPage {
     private PasswordField txtPassword;
 
     @FXML
-    private TextField txtUsername;
-    
+    private TextField txtEmail;
+
     private Firestore firestore;
     private FirestoreContext firestoreContext;
-    
+    private FirebaseAuth auth;
+
     public LoginPage() {
         firestoreContext = new FirestoreContext();
         firestore = firestoreContext.firebase(); // This will get the Firestore instance
@@ -36,39 +45,109 @@ public class LoginPage {
 
     @FXML
     void loginButton(ActionEvent event) throws IOException {
-        String username = txtUsername.getText();
+        String email = txtEmail.getText();
         String password = txtPassword.getText();
 
         // Validate the fields are not empty
-        if (username.isEmpty() || password.isEmpty()) {
-            showAlert("Login Failed", "Username and password cannot be empty.");
+        if (email.isEmpty() || password.isEmpty()) {
+            showAlert("Login Failed", "Email and password cannot be empty.");
             return;
         }
-        // Attempt to login
-        ApiFuture<QuerySnapshot> future = firestore.collection("users").whereEqualTo("username", username).get();
 
+        // Firebase REST API URL for authentication
+        String url = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBXMtMeMr9BZShTYV-dHF4Yyca-52q7Qds";
+        // Attempt to login
         try {
-            QuerySnapshot querySnapshot = future.get(); // This line blocks and waits for the query to complete.
-            if (!querySnapshot.isEmpty()) {
-                // User found, now check the password
-                String storedPassword = querySnapshot.getDocuments().get(0).getString("password");
-                if (password.equals(storedPassword)) {
-                    // Password matches, login successful
-                    App.setRoot("home");
-                } else {
-                    // Password does not match
-                    showAlert("Login Failed", "Invalid username or password.");
+            URL obj = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+            con.setRequestMethod("POST");
+            con.setRequestProperty("Content-Type", "application/json");
+
+            String urlParameters = "{\"email\":\"" + email + "\", \"password\":\"" + password + "\", \"returnSecureToken\":true}";
+
+            // Send POST request
+            con.setDoOutput(true);
+            con.getOutputStream().write(urlParameters.getBytes(StandardCharsets.UTF_8));
+
+            int responseCode = con.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                System.out.println("HTTP_OK Accepted");
+                // Read the response
+                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
                 }
+                in.close();
+
+                // Parse the response to JSON
+                JSONObject jsonResponse = new JSONObject(response.toString());
+
+                // Get the ID token (and other data if needed)
+                String idToken = jsonResponse.getString("idToken");
+
+                App.setRoot("home");
+
+                // Store ID token HERE to manage session (Zach will implement this)
             } else {
-                // User not found
-                showAlert("Login Failed", "Invalid username or password.");
+                BufferedReader in = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+
+                // Log or print the error response
+                System.out.println("Error response: " + response.toString());
+
+                // Parse the response to JSON for more specific error handling (optional)
+                JSONObject jsonResponse = new JSONObject(response.toString());
+                String errorMessage = jsonResponse.getJSONObject("error").getString("message");
+                System.out.println("Error message: " + errorMessage);
+
+                showAlert("Login Failed", "Error: " + errorMessage); // debugging purposes
             }
-        } catch (InterruptedException | ExecutionException e) {
+        } catch (Exception e) {
             e.printStackTrace();
-            // Handle exceptions
             showAlert("Login Error", "An error occurred while trying to log in.");
         }
 
+//        String username = txtUsername.getText();
+//        String password = txtPassword.getText();
+//
+//        // Validate the fields are not empty
+//        if (username.isEmpty() || password.isEmpty()) {
+//            showAlert("Login Failed", "Username and password cannot be empty.");
+//            return;
+//        }
+//        // Attempt to login
+//        ApiFuture<QuerySnapshot> future = firestore.collection("users").whereEqualTo("username", username).get();
+//
+//        try {
+//            QuerySnapshot querySnapshot = future.get(); // This line blocks and waits for the query to complete.
+//            if (!querySnapshot.isEmpty()) {
+//                // User found, now check the password
+//                String storedPassword = querySnapshot.getDocuments().get(0).getString("password");
+//                if (password.equals(storedPassword)) {
+//                    // Password matches, login successful
+//                    App.setRoot("home");
+//                } else {
+//                    // Password does not match
+//                    showAlert("Login Failed", "Invalid username or password.");
+//                }
+//            } else {
+//                // User not found
+//                showAlert("Login Failed", "Invalid username or password.");
+//            }
+//        } catch (InterruptedException | ExecutionException e) {
+//            e.printStackTrace();
+//            // Handle exceptions
+//            showAlert("Login Error", "An error occurred while trying to log in.");
+//        }
     }
 
     private void showAlert(String title, String content) {
--- src/main/java/csc/buzybees/NewRegisterController.java
+++ src/main/java/csc/buzybees/NewRegisterController.java
@@ -4,18 +4,27 @@
  */
 package csc.buzybees;
 //trying to commit lol
+
 import com.google.api.core.ApiFuture;
+import com.google.api.core.ApiFutureCallback;
+import com.google.api.core.ApiFutures;
 import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.WriteResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.UserRecord;
+import com.google.firebase.auth.UserRecord.CreateRequest;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 /**
@@ -23,7 +32,7 @@ import javafx.scene.control.TextField;
  * @author catherinearevalo
  */
 public class NewRegisterController {
-    
+
     @FXML
     private TextField txtFirstName;
     @FXML
@@ -47,10 +56,10 @@ public class NewRegisterController {
     @FXML
     private TextField txtCity;
     @FXML
-    private TextField txtPassword;
+    private PasswordField txtPassword;
     @FXML
     private Button registerButton;
-    
+
     private Firestore firestore;
     private FirestoreContext firestoreContext;
 
@@ -59,9 +68,39 @@ public class NewRegisterController {
         firestore = firestoreContext.firebase(); // This will get the Firestore instance
     }
 
-    
+    private void storeAdditionalUserDetails(String userId) {
+        User newUser = new User(); // set all additional information for user that IS NOT email and password
+        newUser.setDateOfBirth(txtDob.getText());
+        newUser.setUsername(txtUsername.getText());
+        newUser.setJobPosition(txtJobPosition.getText());
+        newUser.setFirstName(txtFirstName.getText());
+        newUser.setLastName(txtLastName.getText());
+        newUser.setStreetAddress(txtStreetAddr.getText());
+        newUser.setCity(txtCity.getText());
+        newUser.setState(txtState.getText());
+        newUser.setZipCode(txtZipCode.getText());
+        newUser.setPhoneNumber(txtPhoneNum.getText());
+
+        ApiFuture<WriteResult> future = firestore.collection("users").document(userId).set(newUser);
+
+        ApiFutures.addCallback(future, new ApiFutureCallback<WriteResult>() {
+            @Override
+            public void onSuccess(WriteResult result) {
+                // This method is called when user information is written successfully
+                System.out.println("Document snapshot successfully written!");
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                // This method is called on failure
+                System.err.println("Error writing document: " + t.getMessage());
+            }
+        }, Executors.newSingleThreadExecutor());
+
+    }
+
     @FXML
-    void switchToNewMember(ActionEvent event) throws IOException {
+    void switchToNewMember(ActionEvent event) throws IOException, FirebaseAuthException {
         // Check if any field is empty
         if (txtFirstName.getText().isEmpty() || txtLastName.getText().isEmpty()
                 || txtDob.getText().isEmpty() || txtJobPosition.getText().isEmpty()
@@ -76,55 +115,69 @@ public class NewRegisterController {
             alert.setHeaderText(null);
             alert.setContentText("Please fill out all fields.");
             alert.showAndWait();
-            return; // Stop further execution
+        } else {
+            FirebaseAuth auth = FirebaseAuth.getInstance();
+            CreateRequest request = new CreateRequest()
+                    .setEmail(txtEmail.getText())
+                    .setPassword(txtPassword.getText());
+            try {
+                UserRecord userRecord = auth.createUser(request);
+                storeAdditionalUserDetails(userRecord.getUid()); 
+                // User registered successfully in Firebase Authentication
+                App.setRoot("newMember");
+            } catch (FirebaseAuthException e) {
+                // Handle Firebase Authentication exceptions
+                e.printStackTrace();
+                // Show error message to the user
+            }
         }
+
         // Create a new User object with the data from the text fields
-        User newUser = new User(
-            txtFirstName.getText(),
-            txtLastName.getText(),
-            txtDob.getText(),
-            txtJobPosition.getText(),
-            txtStreetAddr.getText(),
-            txtCity.getText(),
-            txtState.getText(),
-            txtZipCode.getText(),
-            txtPhoneNum.getText(),
-            txtEmail.getText(),
-            txtUsername.getText(),
-            txtPassword.getText()
-        );
-
-        
-     // Serialize newUser to a Map to store in Firestore
-        Map<String, Object> userMap = new HashMap<>();
-        userMap.put("firstName", newUser.getFirstName());
-        userMap.put("lastName", newUser.getLastName());
-        userMap.put("dateOfBirth", newUser.getDateOfBirth());
-        userMap.put("jobPosition", newUser.getJobPosition());
-        userMap.put("streetAddress", newUser.getStreetAddress());
-        userMap.put("city", newUser.getCity());
-        userMap.put("state", newUser.getState());
-        userMap.put("zipCode", newUser.getZipCode());
-        userMap.put("phoneNumber", newUser.getPhoneNumber());
-        userMap.put("email", newUser.getEmail());
-        userMap.put("username", newUser.getUsername());
-        userMap.put("password", newUser.getPassword()); // Make sure to hash the password before this step
-
-        // Now, use Firestore to store the newUser object
-        ApiFuture<WriteResult> writeResult = firestore.collection("users").document(newUser.getUsername()).set(userMap);
-
-        // Handle the result of the write operation
-        try {
-            // Block and wait for the Firestore operation to complete and log the result
-            WriteResult result = writeResult.get();
-            System.out.println("User registration successful. Write result: " + result);
-            App.setRoot("newMember");
-            // Here you can call switchToHome() or any other method to proceed
-        } catch (InterruptedException | ExecutionException e) {
-            // This will be called if there is an error during the write
-            e.printStackTrace();
-            // Handle the exception, such as providing user feedback
-        }
-
+//        User newUser = new User(
+//            txtFirstName.getText(),
+//            txtLastName.getText(),
+//            txtDob.getText(),
+//            txtJobPosition.getText(),
+//            txtStreetAddr.getText(),
+//            txtCity.getText(),
+//            txtState.getText(),
+//            txtZipCode.getText(),
+//            txtPhoneNum.getText(),
+//            txtEmail.getText(),
+//            txtUsername.getText(),
+//            txtPassword.getText()
+//        );
+//
+//        
+//     // Serialize newUser to a Map to store in Firestore
+//        Map<String, Object> userMap = new HashMap<>();
+//        userMap.put("firstName", newUser.getFirstName());
+//        userMap.put("lastName", newUser.getLastName());
+//        userMap.put("dateOfBirth", newUser.getDateOfBirth());
+//        userMap.put("jobPosition", newUser.getJobPosition());
+//        userMap.put("streetAddress", newUser.getStreetAddress());
+//        userMap.put("city", newUser.getCity());
+//        userMap.put("state", newUser.getState());
+//        userMap.put("zipCode", newUser.getZipCode());
+//        userMap.put("phoneNumber", newUser.getPhoneNumber());
+//        userMap.put("email", newUser.getEmail());
+//        userMap.put("username", newUser.getUsername());
+//        userMap.put("password", newUser.getPassword()); // Make sure to hash the password before this step
+//
+//        // Now, use Firestore to store the newUser object
+//        ApiFuture<WriteResult> writeResult = firestore.collection("users").document(newUser.getUsername()).set(userMap);
+//
+//        // Handle the result of the write operation
+//        try {
+//            // Block and wait for the Firestore operation to complete and log the result
+//            WriteResult result = writeResult.get();
+//            System.out.println("User registration successful. Write result: " + result);
+//            App.setRoot("newMember");
+//            // Here you can call switchToHome() or any other method to proceed
+//        } catch (InterruptedException | ExecutionException e) {
+//            // This will be called if there is an error during the write
+//            e.printStackTrace();
+//            // Handle the exception, such as providing user feedback
+//        }
     }
 }
